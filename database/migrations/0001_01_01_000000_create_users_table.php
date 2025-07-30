ef35363ff0627c9c5555956864b23833406db5d2
rjbelleza <rjbelleza19@gmail.com>
2025-07-30 22:07:36 +0800

    Create: User Seeder

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'customer', 'courier'])->default('customer');
+            $table->boolean('isActive')->default(true);
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin'
+        ]);
+
+        User::create([
+            'email' => 'customer@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 'customer',
+        ]);
+
+        User::create([
+            'email' => 'courier@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 'courier',
+        ]);
+    }
+}
